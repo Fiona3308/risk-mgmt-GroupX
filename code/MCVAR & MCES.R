ff29aad78d13f9276f39d0425b1b702931a46c70
XiaojieWei <weixiaojie7782@163.com>
2018-11-29 18:48:23 -0500

    update

---
+++ code/MCVAR & MCES.R
@@ -0,0 +1,44 @@
+
+# s0: Initial portfolio value
+# mu: Drift
+# sigma: Volatility
+# p: 
+# t: Default 5/252
+# npaths: 
+
+# MONTE CARLO
+Monte_VaR <- function(s0, mu, sigma, p, t, npaths){
+  MCVaR <- NULL
+  len <- length(mu)
+  for (j in 1:len){
+    mc <- NULL
+    for (i in 1:npaths){
+      random <- rnorm(1,0,sqrt(t))
+      temp <- s0 - (s0 * exp((sigma[j] * t ^ 2 * pnrom(1-p)) 
+                             + (mu[j]+random-sigma[j] ^ 2 / 2) * t))
+      # temp <- s0 - (s0 * exp((mu[j] - sigma[j] ^ 2 / 2) * t + sigma[j] * random))
+      mc <- c(mc, temp)
+    }
+    MCVaR <- c(MCVaR, quantile(mc, p, na.rm = TRUE))
+  }
+  return(MCVaR)
+}
+
+Monte_ES <- function(s0, mu, sigma, p, t, npaths){
+  MCES <- NULL
+  len <- length(mu)
+  for (j in 1:len){
+    mc <- NULL
+    for (i in 1:npaths){
+      random <- rnorm(1,0,sqrt(t))
+      temp <- s0 - (s0 * exp((sigma[j] * t ^ 2 * pnrom(1-p)) 
+                             + (mu[j]+random-sigma[j] ^ 2 / 2) * t))
+      # temp <- s0 - (s0 * exp((mu[j] - sigma[j] ^ 2 / 2) * t + sigma[j] * random))
+      mc <- c(mc, temp)
+    }
+    es <- mc[mc > quantile(mc, p, na.rm = TRUE)]
+    ESvalue <- mean(es)
+    MCES <- c(MCES, ESvalue)
+  }
+  return(MCES)  
+}
--- code/gbmES.R
+++ code/gbmES.R
@@ -1,9 +1,9 @@
-gbmES<-function(v0,mu,sigma,p,t){
+gbmES <- function(v0,mu,sigma,p,t){
   #v0 = Initial portfolio value
   #mu = Drift
   #sigma = Volatility.
   #p = Percentile at which to compute VaR.
-  #t = Horizon at which to compute VaR (in years).
+  #t = Horizon at which to compute VaR (in years).(eg,1/252,5/252)
   #ES = Expected shortfall of portfolio (Assumes portfolio follows GBM)
   
   ES <- v0*(1-exp(mu*t)/(1-p)*pnorm(qnorm(1-p)-sigma*sqrt(t)))
