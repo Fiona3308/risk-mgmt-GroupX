bfef3a367cc818fff8d60dd4cec6b51d0d38cffa
XiaojieWei <weixiaojie7782@163.com>
2018-12-12 13:53:51 -0500

    update portfolio HVaR

--- code/portfolio_price.R
+++ code/portfolio_price.R
@@ -1,5 +1,6 @@
 # wgt <- c("0.5","o.3","0.1","0.1")
 # p_price <- cbind(s1,s2,call,put)
+# Assume weights are kept the same of each position each day in the portfilio
 
 comb.col <- function(dat1, dat2) {
   n.row <- min(length(dat1),length(dat2))
@@ -8,17 +9,19 @@ comb.col <- function(dat1, dat2) {
   cbind(dat1, dat2)
 }
 
-portfolio_px <- function(prices,wgts,positions){
+portfolio_px <- function(prices,wgts,positions,v0){
   port_px <- NA
   for(i in 1:length(positions)){
-      if (positions[i] == "long"){
+      if (positions[i] == "short"){
         wgts[i] <- (-1)*wgts[i]
       }else{
     wgts[i]
-  }
-  for(j in 1:nrow(prices)){
-    port_px[j] <- sum(prices[j,]*wgts)
-  }
+      }
+    
+    for(j in 1:nrow(prices)){
+    num <-  wgt*v0/prices[1,]
+    port_px[j] <- sum(prices[j,]*num)
+    }
   }
   return(port_px)
 }
